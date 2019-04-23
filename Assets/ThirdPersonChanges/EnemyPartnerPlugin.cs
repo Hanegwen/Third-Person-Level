8fa15e1c699c1ed042f5d64cb0a513fcf07d3788
Mike Morrone <Hanegwen725@gmail.com>
2019-04-22 19:59:17 -0500

    AI Tweaking

---
+++ Assets/ThirdPersonChanges/EnemyPartnerPlugin.cs
@@ -0,0 +1,32 @@
+﻿using CoverShooter;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPartnerPlugin : MonoBehaviour
+{
+    [SerializeField]
+    CharacterMotor friend;
+
+    bool isActive = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!friend.IsAlive)
+        {
+            if(!isActive)
+            {
+                isActive = !isActive;
+                this.gameObject.GetComponent<AISight>().FieldOfView = 360;
+                this.gameObject.GetComponent<AISight>().Distance = 35;
+                this.gameObject.GetComponent<CharacterAlerts>().OnDead();
+            }
+        }
+    }
+}
